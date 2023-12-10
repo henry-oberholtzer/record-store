7be97bb9114ac9ae157fc7cc2815508a06aadcb0
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-12-10 12:06:07 -0800

    change vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<REPO>/'
+  base: 'https://henry-oberholtzer.github.io/inventory-tracker/'
 })
